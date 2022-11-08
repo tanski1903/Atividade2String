b74ac255d3677235d9194695a4b1a115b17acd87
MatheusBrasil19 <matheusbrasil862@gmail.com>
2022-11-08 19:46:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Atividade 2 Função String</title>
+</head>
+<body>
+    <Form action="atividade2.php" method="post">
+        <h1>CEP</h1>
+        <br><br>
+        <label for="c">Insira seu CEP</label>
+        <br><br>
+        <input type="text" id="c" name="c" minlength="8"/>
+        <br><br>
+        <input type="submit" value="Resultado">
+    </Form>
+    
+</body>
+</html>
